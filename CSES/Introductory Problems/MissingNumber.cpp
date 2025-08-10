7b31093b8c3d389429e42039394aedba67fa3b44
Vansh Dokania <vansh24600@iiitd.ac.in>
2025-08-11 01:08:37 +0530

    Update MissingNumber.cpp

--- CSES/Introductory Problems/MissingNumber.cpp
+++ CSES/Introductory Problems/MissingNumber.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n;
+    cin>>n;
+    map<int, int> mp;
+    vector<int> nums;
+    for(int i=1;i<=n;i++) mp[i]=0;
+    for(int i=0;i<n;i++){
+        int x;
+        cin>>x;
+        nums.push_back(x);
+        mp[x]++;
+    }
+    for(auto x:mp){
+        if(x.second==0){
+            cout<<x.first;
+            break;
+        }
+    }
     return 0;
 }
